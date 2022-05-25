6d483978a2de5840a0f6d4d6fcae72a1e24eeaf6
Samperc <santiperezmonge@gmail.com>
2022-05-25 20:08:50 -0300

    aver

---
+++ Myapp/clases.cs
@@ -0,0 +1,17 @@
+public enum Cargos
+{
+   Auxiliar,Ingeniero,Administrativo,Especialista,Investigador
+}
+
+public class Empleado{
+
+public string Nombre;
+public string Apellido;
+public DateTime Fecnac;
+
+public char EstadoCivil;
+public char genero;
+public DateTime FecIngreso;
+public Double SueldoBasico;
+Cargos Cargo;
+}
