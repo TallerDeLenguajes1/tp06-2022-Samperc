26cd8ee5dc0f25136f4825dc6bd9644ea9de2d93
Samperc <santiperezmonge@gmail.com>
2022-05-27 19:52:22 -0300

    com2

--- Myapp/Program.cs
+++ Myapp/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Empleado[] empleados = new Empleado[3];
+
+
+for (int i = 0; i < 3; i++)
+{
+    empleados[i] = new Empleado();
+    empleados[i].FecIngreso = Convert.ToDateTime(Console.ReadLine()); 
+    empleados[i].FecNac = Convert.ToDateTime(Console.ReadLine());
+    empleados[i].Nombre = Console.ReadLine();
+    empleados[i].Apellido = Console.ReadLine();
+    empleados[i].EstadoCivil = char.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese un numero del 1 al 5");
+    empleados[i].Cargo=(Cargos)Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(empleados[i].Cargo);
+    Console.WriteLine(empleados[i].FecIngreso);
+}
--- Myapp/clases.cs
+++ Myapp/clases.cs
@@ -1,17 +1,66 @@
 public enum Cargos
 {
-   Auxiliar,Ingeniero,Administrativo,Especialista,Investigador
+    Auxiliar , Ingeniero, Administrativo, Especialista, Investigador
 }
 
-public class Empleado{
+public class Empleado
+{
 
-public string Nombre;
-public string Apellido;
-public DateTime Fecnac;
+    public string Nombre;
+    public string Apellido;
+    public DateTime FecNac;
 
-public char EstadoCivil;
-public char genero;
-public DateTime FecIngreso;
-public Double SueldoBasico;
-Cargos Cargo;
+    public char EstadoCivil;
+    public char genero;
+    public DateTime FecIngreso;
+    public Double SueldoBasico;
+    public Cargos Cargo;
+
+    public void Empleado(){
+        FecIngreso = Convert.ToDateTime(Console.ReadLine()); 
+    }
+    public int Antiguedad()
+    {
+        return Convert.ToInt32(DateTime.Now - FecIngreso);
+    }
+    public int edad()
+    {
+
+        return Convert.ToInt32(DateTime.Now - FecNac);
+    }
+    public int Ajubliar()
+    {
+        if (genero == 'M')
+        {
+            return (65 - edad());
+        }
+        else
+        {
+            return (60 - edad());
+        }
+    }
+
+    public double salario(double S)
+    {
+
+        double adicional;
+        if (Antiguedad() > 20)
+        {
+            adicional = S * 0.25;
+        }
+        else
+        {
+            adicional = S * (Antiguedad() / 100);
+        }
+        if (Cargo == Cargos.Ingeniero || Cargo == Cargos.Especialista)
+        {
+            adicional = adicional * 1.50;
+
+        }
+        if (EstadoCivil == 'C')
+        {
+            adicional = adicional + 15000;
+        }
+        return S + adicional;
+    }
 }
